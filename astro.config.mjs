68318a9204394fbf6396a8f67ca49b39e70be2fa
Eloi Torrents <eloitor@disroot.org>
2025-06-10 10:02:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eloitor.github.io/homepage',
-  base: '/academic-homepage-typst/',
+  base: '/homepage/',
   
   integrations: [
     sitemap({
